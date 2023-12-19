3b5ee8ec42ecf14f880452a247cbfbbc6407b483
TechyNome <syedakazmi94@outlook.com>
2023-12-20 04:03:14 +0500

    work

--- POSWork/Admin/ProfitReport.aspx
+++ POSWork/Admin/ProfitReport.aspx
@@ -1,3 +1,15 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Admin/Admin.Master" AutoEventWireup="true" CodeBehind="ProfitReport.aspx.cs" Inherits="POSWork.Admin.ProfitReport" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <asp:Label ID="Label1" runat="server" Text="Total Revenue: "></asp:Label>
+     <asp:Label ID="lbRevenue" runat="server" Text=""></asp:Label>
+    <br />
+     <asp:Label ID="Label2" runat="server" Text="Total Expenses: "></asp:Label>
+     <asp:Label ID="LbExpenses" runat="server" Text=""></asp:Label>
+   
+          <br />
+       <asp:Label ID="Label3" runat="server" Text="Total Profit minus expenses: "></asp:Label>
+     <asp:Label ID="Profit" runat="server" Text=""></asp:Label>
+
 </asp:Content>
--- POSWork/Admin/ProfitReport.aspx.cs
+++ POSWork/Admin/ProfitReport.aspx.cs
@@ -9,9 +9,29 @@ namespace POSWork.Admin
 {
     public partial class ProfitReport : System.Web.UI.Page
     {
+        DataClasses1DataContext db = new DataClasses1DataContext();
         protected void Page_Load(object sender, EventArgs e)
         {
+            
+            if (!Page.IsPostBack)
+            {
+                
 
+                mygrid();
+            }
+
+           
+            }
+        public void mygrid()
+        {
+            var SalesRevenue = db.TbSaleInvoices.Select(x => x.AmountAfterDiscount).Sum();
+            var expenses = db.TbExpenses.Select(x => x.TotalPrice).Sum();
+
+            var profit = SalesRevenue - expenses;
+            lbRevenue.Text = SalesRevenue.ToString();
+
+            LbExpenses.Text = expenses.ToString();
+            Profit.Text = profit.ToString();
         }
     }
 }
